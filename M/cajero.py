c30ff17edda1e639adc31153ebdaedac9fa84e39
Zolagos <smmerino@outlook.com>
2026-03-01 22:20:45 -0500

    ejercicio completado

--- M/cajero.py
+++ M/cajero.py
@@ -10,18 +10,20 @@ Tu saldo en cuenta 1 es: {saldo_cuenta1}
 Tu saldo en cuenta 2 es: {saldo_cuenta2}
 Actualmente te encuentras en la cuenta {cuenta_actual}
         """)
+
+        opcion = input("\nQue desea hacer\n 1=Retirar\n 2=Depositar\n 3=Cambiar de cuenta\n 4=Salir\n")
+
         if cuenta_actual == 1:
             saldo = saldo_cuenta1
         else:
             saldo = saldo_cuenta2
-
-        opcion = input("\nQue desea hacer\n 1=Retirar\n 2=Depositar\n 3=Cambiar de cuenta\n 4=Salir\n")
+            
         if opcion == "1":
 
             ret = float(input("¿Cuanto desea retirar?: "))
 
             saldo_total = saldo_cuenta1 + saldo_cuenta2
-            ret_4 = ret + (ret*CUATROMIL)
+            ret_4 = ret + (ret*CUATROPMIL)
 
             if ret > saldo_total:
                 print("No tienes dinero suficiente en ambas cuentas")
@@ -33,16 +35,18 @@ Actualmente te encuentras en la cuenta {cuenta_actual}
 
             if ret_4 > saldo:
                 
+                print(f"el saldo de la cuenta {cuenta_actual} no es suficiente, se completara con la otra cuenta")
+                
                 if cuenta_actual == 1:
-                    saldo_cuenta1 = 0
-                    saldo_cuenta2 = saldo_cuenta2 - ret_4 - saldo
+                    saldo_cuenta2 = saldo_cuenta2 - (ret_4 - saldo)
                 else:
-                    saldo_cuenta2 = 0
-                    saldo_cuenta1 = saldo_cuenta1 - ret_4 - saldo
-                  
+                    saldo_cuenta1 = saldo_cuenta1 - (ret_4 - saldo)    
+
+                saldo = 0
             else:
                 saldo =  saldo - ret_4
-                print(f"Nuevo saldo de la cuenta {cuenta_actual} es: {saldo}")
+                
+            print(f"Nuevo saldo de la cuenta {cuenta_actual} es: {saldo}")
 
             if cuenta_actual == 1:
                 saldo_cuenta1 = saldo
